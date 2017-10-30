132450ac6d19808c414b1e9b0533b77fcdb4057f
arvind0598 <arvind0598@gmail.com>
2017-10-31 01:37:51 +0530

    Backend now works.

--- AddDetails.java
+++ AddDetails.java
@@ -1,3 +1,7 @@
+import java.util.*;
+import java.io.*;
+import javax.swing.JOptionPane;
+
 public class AddDetails extends javax.swing.JFrame 
 {
 
@@ -5,8 +9,9 @@ public class AddDetails extends javax.swing.JFrame
     {
         initComponents();
     }
-    
-    private void initComponents() {
+
+    private void initComponents() 
+    {
 
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -19,6 +24,7 @@ public class AddDetails extends javax.swing.JFrame
         jTextField3 = new javax.swing.JTextField();
         jTextField4 = new javax.swing.JTextField();
         jFormattedTextField1 = new javax.swing.JFormattedTextField();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -35,9 +41,20 @@ public class AddDetails extends javax.swing.JFrame
         jLabel6.setText("Year :");
 
         jFormattedTextField1.setText("DD.MM.YYYY");
-        jFormattedTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        jFormattedTextField1.addActionListener(new java.awt.event.ActionListener() 
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt) 
+            {
                 jFormattedTextField1ActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Submit");
+        jButton1.addActionListener(new java.awt.event.ActionListener() 
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt) 
+            {
+                jButton1ActionPerformed(evt);
             }
         });
 
@@ -54,33 +71,35 @@ public class AddDetails extends javax.swing.JFrame
                                 .addComponent(jLabel2)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel6)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                            .addComponent(jLabel4)
-                                            .addGap(63, 63, 63))
-                                        .addGroup(layout.createSequentialGroup()
-                                            .addComponent(jLabel6)
-                                            .addGap(74, 74, 74)))
+                                    .addComponent(jLabel5)
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addComponent(jLabel3)
+                                        .addGap(33, 33, 33)))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addGroup(layout.createSequentialGroup()
+                                        .addGap(10, 10, 10)
                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                            .addComponent(jLabel5)
-                                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                                .addComponent(jLabel3)
-                                                .addGap(33, 33, 33)))
-                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                            .addGroup(layout.createSequentialGroup()
-                                                .addGap(10, 10, 10)
-                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                    .addComponent(jFormattedTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                                            .addComponent(jTextField1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(jFormattedTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addComponent(jTextField1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(0, 0, Short.MAX_VALUE))))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(144, 144, 144)
-                        .addComponent(jLabel1)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(8, 8, 8)
+                                .addComponent(jButton1)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addGap(99, 99, 99))
         );
         layout.setVerticalGroup(
@@ -108,15 +127,92 @@ public class AddDetails extends javax.swing.JFrame
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel6)
                     .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(56, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(15, Short.MAX_VALUE))
         );
 
         pack();
-    }
+    }// </editor-fold>//GEN-END:initComponents
 
+    private void jFormattedTextField1ActionPerformed(java.awt.event.ActionEvent evt) 
+    {//GEN-FIRST:event_jFormattedTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jFormattedTextField1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) 
+    {//GEN-FIRST:event_jButton1ActionPerformed
+        Student x = new Student();
+        Boolean valid = true;
+
+        try
+        {
+            x.name = jTextField2.getText();
+            if(x.name.equals(""))
+            valid = false; 
+
+            x.regNo = jTextField1.getText();
+            if(x.regNo.equals("") || Long.parseLong(x.regNo) <= 100000000) valid = false;
+            //this will also throw numberformatexception if invalid regno
+
+            x.DOB = jFormattedTextField1.getText();           
+            if(x.DOB.equals("") || Long.parseLong(x.regNo) <= 1000000) valid = false;
+            //add extra conditions to make sure it is actually a valid DOB
+
+            x.branch = jTextField3.getText();
+            if(x.branch.equals("") || x.branch.length() > 3) valid = false;
+
+            x.year = Integer.parseInt(jTextField4.getText());
+            if(jTextField4.getText().equals("")) valid = false;
+
+            if(!valid) throw new BadInputException();
+        }
+
+        catch(Exception e)
+        {
+            valid = false;
+            JOptionPane.showMessageDialog(this, e, "Error", JOptionPane.ERROR_MESSAGE);
+            x = new Student();
+        }
+
+        ArrayList<Student> data = new ArrayList<Student>();
+
+        try
+        {
+            ObjectInputStream o = new ObjectInputStream(new FileInputStream("database.txt"));
+            while(true)
+            {
+                Student s = (Student)o.readObject();
+                if(s.name.equals("uninit")) break;
+                data.add(s);
+            }
+            o.close();
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(this, e, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+        try
+        {
+            ObjectOutputStream o = new ObjectOutputStream(new FileOutputStream("database.txt"));
+            for(int i = 0; i < data.size(); i++)
+                o.writeObject(data.get(i));
+            o.writeObject(x);
+            o.writeObject(new Student());
+            o.close();
+            if(valid)
+                JOptionPane.showMessageDialog(this, "New Student has been added" , "Added", JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(this, e, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     public static void main(String args[]) 
-    {
+    {         
         try 
         {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) 
@@ -144,7 +240,7 @@ public class AddDetails extends javax.swing.JFrame
         {
             java.util.logging.Logger.getLogger(AddDetails.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        
+    
         java.awt.EventQueue.invokeLater(new Runnable() 
         {
             public void run() 
@@ -153,7 +249,9 @@ public class AddDetails extends javax.swing.JFrame
             }
         });
     }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JFormattedTextField jFormattedTextField1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -166,4 +264,27 @@ public class AddDetails extends javax.swing.JFrame
     private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
     // End of variables declaration//GEN-END:variables
+}
+
+class Student implements Serializable
+{
+    String name;
+    String regNo;
+    String DOB;
+    String branch;
+    int year;
+
+    Student()
+    {
+        name = regNo = DOB = branch = "uninit";
+        year = -1; 
+    }
+}
+
+class BadInputException extends Exception
+{
+    BadInputException()
+    {
+        System.out.println("One or more incorrect entries made. Try again?");
+    }
 }
